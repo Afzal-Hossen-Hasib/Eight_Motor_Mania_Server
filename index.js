f582548bfeda4a480af4ae530c0fdeda17609803
afzalhossenhasib <ahhasib47@gmail.com>
2022-05-07 21:51:35 +0600

    delete handle

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ async function run() {
           app.delete('/inventory/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
-            const result = await inventoryCollection.deleteOne(query);
+            const result = await myItemCollection.deleteOne(query);
             res.send(result);
           });
 
